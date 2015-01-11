52e2c310e065ca9e6e4a390d818be180e6b00135
Mark Kemp <mark@createur.com.au>
2015-01-12 10:53:18 +1100

    removed redundant markup

--- inc-footer.php
+++ inc-footer.php
@@ -1,38 +1,5 @@
     <div class="footer">
       <div class="container">
-        <div class="row">
-          <div class="col-xs-12">
-            <div class="footer-pre">
-              <a class="footer-logo" href="home.php">
-                <img src="img/logo-small.png" alt="Analu">
-              </a>
-              <div class="social">
-                <a href="#"><span class="icon-facebook"></span></a>
-                <a href="#"><span class="icon-pinterest"></span></a>
-                <a href="#"><span class="icon-instagram"></span></a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="footer-sub">
-          <div class="row">
-            <div class="col-xs-12 col-sm-6">
-              <address class="address">
-                <span class="icon-pin"></span> Showroom 1, 19a Boundary Street, Rushcutters Bay, <br class="visible-xs">Sydney, NSW, 2011, Australia
-              </address>
-              <a class="phone" href="tel:0293540662">
-                <span class="icon-phone"></span>+612 8354 0662
-              </a>
-            </div>
-            <div class="col-xs-12 col-sm-6">
-              <ul class="list-inline">
-                <li><a href="content.php">Privacy Policy</a></li>
-                <li><a href="content.php">Terms of Service</a></li>
-                <li><a href="content.php">Returns Policy</a></li>
-                <li><a href="content.php">Shipping Information</a></li>
-              </ul>
-            </div>
-          </div>
-        </div>
+
       </div>
     </div>
--- index.php
+++ index.php
@@ -14,10 +14,11 @@
   </head>
 
   <body class="index">
+    <div class="container">
+      
+    </div>
     <!--[if lt IE 7]>
         <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
     <![endif]-->
-
-
   </body>
 </html>
